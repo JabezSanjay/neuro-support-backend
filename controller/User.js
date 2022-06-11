399fa1bddd5a07bb81617c83e3c15143c83105a9
RishikeshBalan <rishikesh@entrans.io>
2022-06-11 19:30:28 +0530

    smal change

--- controller/User.js
+++ controller/User.js
@@ -1,159 +1,155 @@
-const User = require('../models/User');
-const crypto = require('crypto');
-const BigPromise = require('../middleware/bigPromise');
-const cookieToken = require('../utils/cookieToken');
-const CustomError = require('../utils/customError');
-const validator = require('validator');
-const { uuid } = require('uuidv4');
+const User = require('../models/User')
+const crypto = require('crypto')
+const BigPromise = require('../middleware/bigPromise')
+const cookieToken = require('../utils/cookieToken')
+const CustomError = require('../utils/customError')
+const validator = require('validator')
 
 exports.signup = BigPromise(async (req, res, next) => {
-  let newUser;
-  const { name, email, password } = req.body;
+  let newUser
+  const { name, email, password } = req.body
   if (!name) {
-    return next(new CustomError('Name is required!', 400, res));
+    return next(new CustomError('Name is required!', 400, res))
   }
   if (!email) {
-    return next(new CustomError('Email is required!', 400, res));
+    return next(new CustomError('Email is required!', 400, res))
   }
   if (!password) {
-    return next(new CustomError('Password is required!', 400, res));
+    return next(new CustomError('Password is required!', 400, res))
   }
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email })
   if (user) {
-    return next(new CustomError('User already exists', 400, res));
+    return next(new CustomError('User already exists', 400, res))
   }
-
   newUser = await User.create({
     name,
     email,
     password,
-    socketId: uuid().toString(),
-  });
-  cookieToken(newUser, res);
-});
+  })
+  cookieToken(newUser, res)
+})
 
 exports.signin = BigPromise(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   if (!email) {
-    return next(new CustomError('Email is required!', 400, res));
+    return next(new CustomError('Email is required!', 400, res))
   }
   if (!password) {
-    return next(new CustomError('Password is required!', 400, res));
+    return next(new CustomError('Password is required!', 400, res))
   }
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select('+password')
   if (!user) {
-    return next(new CustomError('User does not exists!', 404, res));
+    return next(new CustomError('User does not exists!', 404, res))
   }
-  const isPasswordValid = await user.validatePassword(password);
+  const isPasswordValid = await user.validatePassword(password)
   if (!isPasswordValid) {
-    return next(new CustomError('Email and password does not match', 400, res));
+    return next(new CustomError('Email and password does not match', 400, res))
   }
-  cookieToken(user, res);
-});
+  cookieToken(user, res)
+})
 
 exports.logout = BigPromise(async (req, res, next) => {
   res.cookie('token', null, {
     expires: new Date(Date.now()),
     httpOnly: true,
-  });
+  })
   res.status(200).json({
     success: true,
     message: 'Logout successfull!',
-  });
-});
+  })
+})
 
 exports.getLoggedInUserInfo = BigPromise(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
+  const user = await User.findById(req.user.id)
   res.status(200).json({
     success: true,
     data: user,
     message: 'User info fetched successfully!',
-  });
-});
+  })
+})
 
 //Admin APIs
 exports.adminCreateUser = BigPromise(async (req, res, next) => {
-  const { name, email, password, role } = req.body;
+  const { name, email, password, role } = req.body
   if (!name) {
-    return next(new CustomError('Name is required!', 400, res));
+    return next(new CustomError('Name is required!', 400, res))
   }
   if (!email) {
-    return next(new CustomError('Email is required!', 400, res));
+    return next(new CustomError('Email is required!', 400, res))
   }
   if (!password) {
-    return next(new CustomError('Password is required!', 400, res));
+    return next(new CustomError('Password is required!', 400, res))
   }
   if (!role) {
-    return next(new CustomError('Role is required!', 400, res));
+    return next(new CustomError('Role is required!', 400, res))
   }
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email })
   if (user) {
-    return next(new CustomError('User already exists', 400, res));
+    return next(new CustomError('User already exists', 400, res))
   }
   const newUser = await User.create({
     name,
     email,
     password,
     role,
-    socketId: uuid().toString(),
-  });
+  })
   res.status(201).json({
     success: true,
     data: newUser,
     message: 'User created successfully!',
-  });
-});
+  })
+})
 
 exports.adminReadUsers = BigPromise(async (req, res, next) => {
-  const users = await User.find();
+  const users = await User.find()
   res.status(200).json({
     success: true,
     data: users,
     message: 'Users fetched successfully!',
-  });
-});
+  })
+})
 
 exports.adminDeleteOneUser = BigPromise(async (req, res, next) => {
-  const user = await User.findByIdAndDelete(req.params.id);
+  const user = await User.findByIdAndDelete(req.params.id)
   if (!user) {
-    return next(new CustomError('User does not exists!', 404, res));
+    return next(new CustomError('User does not exists!', 404, res))
   }
   res.status(200).json({
     success: true,
 
     message: 'User deleted successfully!',
-  });
-});
+  })
+})
 
 exports.adminAddMentorToUsers = BigPromise(async (req, res, next) => {
-  const { mentorId } = req.body;
+  const { mentorId, studentId } = req.body
   if (!mentorId) {
-    return next(new CustomError('Mentor is required!', 400, res));
+    return next(new CustomError('Mentor is required!', 400, res))
   }
-  const user = await User.find({
-    socketId: req.params.id,
-  });
+  const user = await User.findOne({
+    socketId: studentId,
+  })
   if (!user) {
-    return next(new CustomError('User does not exists!', 404, res));
+    return next(new CustomError('User does not exists!', 404, res))
   }
-  user.mentorsId.push(mentorId);
-  await user.save();
+  user.mentorsId.push(mentorId)
+  await user.save()
   //add users to mentor id
-  const mentor = await User.find({
+  const mentor = await User.findOne({
     socketId: mentorId,
-  });
+  })
   if (!mentor) {
-    return next(new CustomError('Mentor does not exists!', 404, res));
+    return next(new CustomError('Mentor does not exists!', 404, res))
   }
-  mentor.studentsId.push(user._id);
-  await mentor.save();
+  mentor.studentsId.push(studentId)
+  await mentor.save()
 
   if (!user) {
-    return next(new CustomError('User does not exists!', 404, res));
+    return next(new CustomError('User does not exists!', 404, res))
   }
   res.status(200).json({
     success: true,
     data: user,
     message: 'Mentor added successfully!',
-  });
-});
+  })
+})

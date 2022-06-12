2171847efb48e789a35020e1da5a7bf1cdb65c86
JabezSanjay <jbzsanjay2000@gmail.com>
2022-06-12 09:43:00 +0530

    Bug fixes

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -5,7 +5,6 @@ const cookieToken = (user, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
   };
   user.password = undefined;
   res.status(200).cookie('token', token, options).json({
